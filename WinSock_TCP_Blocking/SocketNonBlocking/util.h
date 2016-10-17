73aee55a3f4b547aabe3cf18cafacf8f8027fc64
remixgit <mihailo.vasiljevic93@gmail.com>
2016-10-17 19:31:20 +0200

    Better struct.

--- WinSock_TCP_Blocking/SocketNonBlocking/util.h
+++ WinSock_TCP_Blocking/SocketNonBlocking/util.h
@@ -5,10 +5,12 @@
 typedef struct buffer {
 	char *name;
 	char *data;
+	int pushIdx;
+	int popIdx;
 } Buffer;
 
-void add(Buffer buffer, char *data, int pushIdx, int popIdx, int count);            // adding data to the buffer
-void remove(Buffer buffer, char *data, int pushIdx, int popIdx, int count);         // removing data from the buffer
+void add(Buffer buffer, char *data, int count);            // adding data to the buffer
+void remove(Buffer buffer, char *data, int count);         // removing data from the buffer
 void expand(Buffer buffer);					   // expand the buffer size
 void shrink(Buffer buffer);                     // shrink the buffer size when it's fulness is less or equal 25%
 int isEnough(int dataLength, int bufferLength);  //is enough place for data in buffer
