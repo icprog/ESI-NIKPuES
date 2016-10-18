f3330c768d4b2e7678329bb45d83b70299369c5d
Davor Na─æ <RA64-2012@ftn.ns>
2016-10-18 15:33:04 +0200

    Ring bafer remove and expand

--- WinSock_TCP_Blocking/SocketNonBlocking/util.cpp
+++ WinSock_TCP_Blocking/SocketNonBlocking/util.cpp
@@ -1,3 +1,59 @@
 #include "util.h"
+#include <stdlib.h>
 
 
+int remove(Buffer * buffer, char * data, int dataSize)
+{
+	//ako se poklapaju pop i pushIdx, nemamo podataka
+	/*if (buffer->popIdx == buffer->pushIdx) {
+		return -1; //vrati gresku
+	}*/
+
+
+	if (buffer->count == 0) {
+		return false;
+	}
+
+	for (int i = 0; i < dataSize; i++) {
+
+		data[i] = buffer->data[buffer->popIdx];
+
+		buffer->popIdx ++;
+
+		if (buffer->popIdx == buffer->size) {
+			buffer->popIdx = 0;
+		}
+	}
+
+	buffer->count -= dataSize;
+
+}
+
+void expand(Buffer * buffer, int howMuch)
+{
+	char* newData;
+	int newSize;
+
+	if (howMuch == 0) { 
+		newSize = buffer->size * 2; 
+		newData = (char *)malloc(sizeof(char)*newSize);
+	}
+	else {
+		newSize = buffer->size * howMuch;
+		newData = (char *)malloc(sizeof(char)*newSize);
+	}
+
+	//kada povecamo moramo da premestimo podatke sa kraja starog buffer-a na pocetak novog buffer-a
+	for (int i = 0; i < buffer->count; i++) {
+		if (buffer->popIdx == buffer->size)
+			buffer->popIdx = 0;
+
+		newData[i] = buffer->data[i];
+	}
+
+	buffer->data = newData;
+	buffer->size = newSize;
+	buffer->pushIdx = buffer->count;
+	buffer->popIdx = 0;
+
+}
--- WinSock_TCP_Blocking/SocketNonBlocking/util.h
+++ WinSock_TCP_Blocking/SocketNonBlocking/util.h
@@ -7,16 +7,15 @@ typedef struct buffer {
 	char *data;
 	int pushIdx;
 	int popIdx;
+	int count;
+	int size;
 } Buffer;
 
-void add(Buffer buffer, char *data, int count);            // adding data to the buffer
-void remove(Buffer buffer, char *data, int count);         // removing data from the buffer
-void expand(Buffer buffer);					   // expand the buffer size
-void shrink(Buffer buffer);                     // shrink the buffer size when it's fulness is less or equal 25%
+int add(Buffer *buffer, char *data, int dataSize);            // adding data to the buffer
+int remove(Buffer *buffer, char *data, int dataSize);         // removing data from the buffer
+void expand(Buffer *buffer, int howMuch);					   // expand the buffer size
+void shrink(Buffer *buffer);                     // shrink the buffer size when it's fulness is less or equal 25%
 int isEnough(int dataLength, int bufferLength);  //is enough place for data in buffer
 int fullness(Buffer buffer);                    // returns fullnes in procents( 25 means 25%)
-int size(Buffer buffer);                        // returns the size of the buffer
-int count(Buffer buffer);                       // returns the number of elements currently in buffer
-int pushIdx(Buffer buffer);                     // returns current position of pushIdx
-int popIdx(Buffer buffer);                      // returns current position of popIdx
+
 /* CIRCULAR BUFFER INTERFACE */
