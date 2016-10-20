43389fa8a2346b076dece4c8f8cb2a6ff5424727
Davor Na─æ <RA64-2012@ftn.ns>
2016-10-20 19:28:03 +0200

    Merge

--- WinSock_TCP_Blocking/WinSockClient/Client.cpp
+++ WinSock_TCP_Blocking/WinSockClient/Client.cpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <conio.h>
 //#include "C:/Users/ra64-2012/Desktop/Blok1/ESI-NIKPuES/WinSock_TCP_Blocking/SocketNonBlocking/socketNB.h" //davor
-#include "C:/Users/RA4-2012/Documents/ESI-NIKPuES/WinSock_TCP_Blocking/SocketNonBlocking/socketNB.h"
+#include "C:/Users/RA64-2012/Desktop/20.10/ESI-NIKPuES/WinSock_TCP_Blocking/SocketNonBlocking/socketNB.h"
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT 27016
 
@@ -15,86 +15,102 @@
 // Returns true if succeeded, false otherwise.
 bool InitializeWindowsSockets();
 
-int __cdecl main(int argc, char **argv) 
+int __cdecl main(int argc, char **argv)
 {
-    // socket used to communicate with server
-    SOCKET connectSocket = INVALID_SOCKET;
-    // variable used to store function return value
-    int iResult;
-    // message to send
-    char *messageToSend = "this is a test";
-    
-    // Validate the parameters
-    if (argc != 2)
-    {
-        printf("usage: %s server-name\n", argv[0]);
-        return 1;
-    }
+	// socket used to communicate with server
+	SOCKET connectSocket = INVALID_SOCKET;
+	// variable used to store function return value
+	int iResult;
+	// message to send
+	char *messageToSend = "this is a test";
+	/////////////////////////////////////////////////////////////////////////////////
+	char *data = (char *)malloc(sizeof(char) * 26);
+	memset(data, 0, 26);
+	data[0] = 26;
 
-    if(InitializeWindowsSockets() == false)
-    {
+	*(char*)((int *)data + 1) = 4;
+	char *ime = "RED1";
+	char *message = "ovo je poruka";
+
+	memcpy(data + 8, ime, 4);
+	memcpy(data + 12, message, 14);
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// Validate the parameters
+	if (argc != 2)
+	{
+		printf("usage: %s server-name\n", argv[0]);
+		return 1;
+	}
+
+	if (InitializeWindowsSockets() == false)
+	{
 		// we won't log anything since it will be logged
 		// by InitializeWindowsSockets() function
 		return 1;
-    }
+	}
 
-    // create a socket
-    connectSocket = socket(AF_INET,
-                           SOCK_STREAM,
-                           IPPROTO_TCP);
+	// create a socket
+	connectSocket = socket(AF_INET,
+		SOCK_STREAM,
+		IPPROTO_TCP);
 
-    if (connectSocket == INVALID_SOCKET)
-    {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
-    }
+	if (connectSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
 
-    // create and initialize address structure
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
-    serverAddress.sin_port = htons(DEFAULT_PORT);
-    // connect to server specified in serverAddress and socket connectSocket
-    if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
-    {
-        printf("Unable to connect to server.\n");
-        closesocket(connectSocket);
-        WSACleanup();
-    }
- 
+	// create and initialize address structure
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
+	serverAddress.sin_port = htons(DEFAULT_PORT);
+	// connect to server specified in serverAddress and socket connectSocket
+	if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+	{
+		printf("Unable to connect to server.\n");
+		closesocket(connectSocket);
+		WSACleanup();
+	}
+
 	// Set socket to nonblocking mode
 	unsigned long int nonBlockingMode = 1;
 	iResult = ioctlsocket(connectSocket, FIONBIO, &nonBlockingMode);
 
-    // Send an prepared message with null terminator included
-	iResult = SEND(connectSocket, messageToSend);
+	// Send an prepared message with null terminator included
+	iResult = SEND(connectSocket, data);
 
-    if (iResult == SOCKET_ERROR)
-    {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(connectSocket);
-        WSACleanup();
-        return 1;
-    }
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("send failed with error: %d\n", WSAGetLastError());
+		closesocket(connectSocket);
+		WSACleanup();
+		return 1;
+	}
 
-    printf("Bytes Sent: %ld\n", iResult);
+	printf("Bytes Sent: %ld\n", iResult);
 
-    // cleanup
-    closesocket(connectSocket);
-    WSACleanup();
+	// cleanup
+	closesocket(connectSocket);
+	WSACleanup();
 
-    return 0;
+	free(data);
+	return 0;
 }
 
 bool InitializeWindowsSockets()
 {
-    WSADATA wsaData;
+	WSADATA wsaData;
 	// Initialize windows sockets library for this process
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
-    {
-        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
-        return false;
-    }
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return false;
+	}
 	return true;
 }
